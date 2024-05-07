637f4342f042154d5f020e809503c7655b932996
uranotakashi <takashiurano1@outlook.jp>
2024-05-07 17:28:17 +0900

    修正-2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
     port:                  587,
     domain:               'gmail.com',
     user_name:            'uranotakashi19900620@gmail.com', 
-    password:              Rails.application.credentials.mailer[:password],
+    password:              Rails.application.credentials.amailer['twoirdikknkoesis'],
     authentication:       'plain',
     enable_starttls_auto:  true
   }
